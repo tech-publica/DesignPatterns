6867d0ba0c4b3ac14245412de379187edb265737
Riccardo Audano <riccardo.at.work@gmail.com>
2018-01-10 21:25:09 +0100

    Added singleton example and test

---
+++ DesignPatterns/Creational/Singleton/Highlander.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    //there can be only one :)
+    public class Highlander
+    {
+        private static Lazy<Highlander> instance = 
+            new Lazy<Highlander>(() => new Highlander(), true);
+
+        public static Highlander Instance
+        {
+            get
+            {           
+                return instance.Value;
+            }
+        }
+        private Highlander()
+        {
+
+        }
+
+        public static void ShowSpecialEffects()
+        {
+
+        }
+
+        public void SaveTheWorld()
+        {
+
+        }
+    }
+}
---
+++ DesignPatternsExamplesTest/Creational/Singleton/SingletonTest.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns.Creational.Singleton;
+using Xunit;
+
+namespace DesignPatternsExamplesTest.Creational.Singleton
+{
+    public class SingletonTest
+    {
+        [Fact]
+        public void RequestSingletonTest()
+        {
+            Highlander connor = Highlander.Instance;
+            Highlander another = Highlander.Instance;
+            Assert.Same(connor, another);
+        }
+    }
+}
